b53c241b491b41d23f84630669e3e8c92c01fa93
NQL <68323061+linhhhhaksh@users.noreply.github.com>
2022-06-02 23:29:36 +0700

    add new variable for BT1

--- src/cmc/entrytest/linh/BT1.java
+++ src/cmc/entrytest/linh/BT1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BT1 {
     public static void main(String[] args) {
        int Nam;
+       int b;
        Scanner scan = new Scanner(System.in);
         System.out.println("Nhap vao nam ");
         Nam=scan.nextInt();
